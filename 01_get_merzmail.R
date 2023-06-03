c85bc28b3ea02638a0a22c7c0933034c03eed5b3
antonknk <antonkoennekemail@yahoo.de>
2023-06-03 23:41:27 +0200

    include date and header of post

--- 01_get_merzmail.R
+++ 01_get_merzmail.R
@@ -20,16 +20,34 @@ get_mm <- function(baseurl = "https://www.friedrich-merz.de/merzmail/merzmail-",
     
     x <- lapply(urls, function(y) {
       
-      y %>%
-        read_html() %>%
+      html <- read_html(y)
+      
+      result <- list()
+      
+      result$fulltext <- html %>%
         html_element(xpath = '/html/body/div[2]/div/section/div/div/div/div/div/div[4]/div') %>%
         html_text()
       
-      Sys.sleep(wait)
+      result$date <- html %>% 
+        html_element(xpath = "/html/body/div[2]/div/section/div/div/div/div/div/div[3]/div/ul/li/span") %>% 
+        html_text2()
+      
+      result$title <- html %>% 
+        html_element(xpath = "/html/body/div[2]/div/section/div/div/div/div/div/div[1]/div/span") %>% 
+        html_text()
+      
+      Sys.sleep(wait) 
+      
+      return(result)
     })
     
     return(x)
 }
 
-mm_1_to_152 <- get_mm(index_from = 1, index_to = 152)
+mm_1_to_152 <- get_mm(index_from = 1, index_to = 2, wait = 1)
+
+"https://www.friedrich-merz.de/merzmail/merzmail-1" %>%
+  read_html() %>%
+  html_element(xpath = "/html/body/div[2]/div/section/div/div/div/div/div/div[3]/div/ul/li/span") %>% 
+  html_text2()
 

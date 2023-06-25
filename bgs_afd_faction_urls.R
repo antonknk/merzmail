a2fee744444e6701e53dcb830f698f8144f2762f
antonknk <antonkoennekemail@yahoo.de>
2023-06-26 01:04:45 +0200

    finish afd press release scrape

--- bgs_afd_faction_urls.R
+++ bgs_afd_faction_urls.R
@@ -3,3 +3,14 @@
 source("get_afd_faction.R")
 # afdbt_urls_bg <- afdbt_get_urls(pages = c(0:750))
 afdbt_press <- get_afdbt_articles(urls = afdbt_urls_bg$urls[4763:6008])
+afd_articles <- read_csv("texts/cache/afd_article_cache.csv")
+
+afdarticles <- afd_articles %>% 
+  unique()
+
+# remove empty articles that only linked legislative docs
+afd_articles <-  afd_articles %>% 
+  filter(!is.na(fulltext))
+
+# save to disk 
+write_csv(afd_articles, "texts/afdbt_articles.csv")

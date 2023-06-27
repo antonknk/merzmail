40ca626ae73659d413a21993fbae62db87cb5a89
antonknk <antonkoennekemail@yahoo.de>
2023-06-27 18:56:46 +0200

    fix

--- bgs_fdpbt_press_releases.R
+++ bgs_fdpbt_press_releases.R
@@ -3,5 +3,6 @@ pacman::p_load(rvest, tidyverse, httr)
 fdp_press_releases <- get_fdpbt_articles(fdp_urls$url)
 
 fdp_press_releases <- fdp_press_releases %>% 
-  distinct(fulltext)
+  filter(!duplicated(fulltext)) %>% 
+  tibble()
 write_csv(fdp_press_releases, "texts/fdpbt_press.csv")
